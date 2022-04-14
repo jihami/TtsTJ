37bd25e0d4032a879d1fbdf35cda197813e725b1
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2022-04-14 09:00:00 +0900

    최종 수정

--- app/src/main/java/kr/hs/emirim/w2027/ttstj/Result.java
+++ app/src/main/java/kr/hs/emirim/w2027/ttstj/Result.java
@@ -34,36 +34,36 @@ public class Result extends AppCompatActivity {
         getWindow().setWindowAnimations(0);
 
         ImageView mImageView = (ImageView) findViewById(R.id.result_img);
-        int imageId = (int) (Math.random() * images.length);
-        mImageView.setBackgroundResource(images[imageId]);
+//        int imageId = (int) (Math.random() * images.length);
+        mImageView.setBackgroundResource(images[maxIndex]);
 
         TextView ResultType = (TextView)findViewById(R.id.result_type);
         TextView ResultText = (TextView)findViewById(R.id.result_text);
 
-        if(images[maxIndex] == R.drawable.result_0){
+        if(maxIndex == 0){
             ResultType.setText(R.string.type_0);
             ResultText.setText(R.string.result_0);
-        }else if(images[maxIndex] == R.drawable.result_1){
+        }else if(maxIndex == 1){
             ResultType.setText(R.string.type_1);
             ResultText.setText(R.string.result_1);
-        }else if(images[maxIndex] == R.drawable.result_2){
+        }else if(maxIndex == 2){
             ResultType.setText(R.string.type_2);
             ResultText.setText(R.string.result_2);
-        }else if(images[maxIndex] == R.drawable.result_3){
+        }else if(maxIndex == 3){
             ResultType.setText(R.string.type_3);
             ResultText.setText(R.string.result_3);
-        }else if(images[maxIndex] == R.drawable.result_4){
+        }else if(maxIndex == 4){
             ResultType.setText(R.string.type_4);
             ResultText.setText(R.string.result_4);
-        }else if(images[maxIndex] == R.drawable.result_5){
+        }else if(maxIndex == 5){
             ResultType.setText(R.string.type_5);
             ResultText.setText(R.string.result_5);
-        }else if(images[maxIndex] == R.drawable.result_6){
+        }else if(maxIndex == 6){
             ResultType.setText(R.string.type_6);
             ResultText.setText(R.string.result_6);
         }
         Button btnR = findViewById(R.id.btn_Result);
-        btnR.setOnClickListener(btnListener);
+        btnR.setOnClickListenerpk(btnListener);
 
     }
     View.OnClickListener btnListener = new View.OnClickListener() {
